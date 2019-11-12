8b765e4be1cf32aa949b543ec421ed3ccdbb179e
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-12 19:22:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,2 +1,27 @@
 class School
+  def initialize (school_name)
+    @school_name=school_name
+  end 
 end 
+
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
+# 
+# BRANDS=[]
+# 
+#   def initialize(brand)
+#     @brand = brand
+#     if BRANDS.include?(brand)
+#     else
+#       BRANDS << brand
+#     end
+#   end
+# 
+# 
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+# 
+# end
