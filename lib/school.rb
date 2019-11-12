3dd77ede36f92dac688c107d30c7096292002a64
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-12 19:47:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,10 +22,9 @@ class School
     @roster[number]
   end
 
-  def sort
-    def sort
-@roster.each do |grade, name|
-@roster[grade] = name.sort
+def sort
+ @roster.each do |grade, name|
+  @roster[grade] = name.sort
 end
 
 end
