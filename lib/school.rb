cf82d8ca350f58db4057f1c3da28f3a4c9bfca0e
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-12 19:39:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,21 @@
 class School
 
-  def initialize (name)
-    @name=name
+  def initialize (school_name)
+    @school_name=school_name
     @roster={}
   end
 
   def roster
     @roster
   end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+    
 end
