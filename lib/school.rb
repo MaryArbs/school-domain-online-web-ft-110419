5593090ea5b9f43cf7ed7c092a630bc0b5c47544
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-12 19:32:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,5 @@
 class School
-  attr_accessor :class, :grade
+  
   def initialize (name)
     @name=name
     @roster={}
@@ -9,25 +9,3 @@ class School
     @roster
   end
 end
-
-# class Shoe
-#   attr_accessor :color, :size, :material, :condition
-#   attr_reader :brand
-#
-# BRANDS=[]
-#
-#   def initialize(brand)
-#     @brand = brand
-#     if BRANDS.include?(brand)
-#     else
-#       BRANDS << brand
-#     end
-#   end
-#
-#
-#   def cobble
-#     self.condition = "new"
-#     puts "Your shoe is as good as new!"
-#   end
-#
-# end
